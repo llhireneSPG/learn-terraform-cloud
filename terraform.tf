1d5e10b9290f2a5b5b2b0ec6df14c88066360538
Irene Lim <irene.lim@ihsmarkit.com>
2023-06-21 14:42:00 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "SPGlobal-llhirene"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
